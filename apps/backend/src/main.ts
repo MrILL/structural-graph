7bd3fca1722adc74bf019f8e4a08a8910b2e4381
MrILL <ilua2403@ukr.net>
2022-07-16 23:20:57 +0300

    feat: init backend

---
+++ apps/backend/src/main.ts
@@ -0,0 +1,17 @@
+import { Logger } from '@nestjs/common'
+import { NestFactory } from '@nestjs/core'
+import { ConfigService } from '@nestjs/config'
+
+import { AppModule } from './modules/app/app.module'
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule)
+  app.setGlobalPrefix('api')
+
+  const configService = app.get(ConfigService)
+  const port = configService.get('PORT') || 3000
+  await app.listen(port)
+  Logger.log(`🚀 Application is running on: http://localhost:${port}/api`)
+}
+
+bootstrap()
