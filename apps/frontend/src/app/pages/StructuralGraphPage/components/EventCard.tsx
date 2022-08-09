8a5d366566269b76ac0bdddf5feaea8a5d3d006c
MrILL <ilua2403@ukr.net>
2022-08-09 23:19:53 +0300

    feat(frontend): init image placeholder

--- apps/frontend/src/app/pages/StructuralGraphPage/components/EventCard.tsx
+++ apps/frontend/src/app/pages/StructuralGraphPage/components/EventCard.tsx
@@ -2,10 +2,14 @@ import styled from 'styled-components'
 
 import { GameEvent } from '@sg/types'
 
+const imagePlaceholder =
+  'https://media.istockphoto.com/vectors/default-image-icon-vector-missing-picture-page-for-website-design-or-vector-id1357365823?k=20&m=1357365823&s=612x612&w=0&h=ZH0MQpeUoSHM3G2AWzc8KkGYRg4uP_kuu0Za8GFxdFc='
+
 export function EventCard({ data }: { data: GameEvent }) {
+  // console.log(data)
   return (
     <Wrapper>
-      <Image src={data.imgUrl} alt="new" />
+      <Image src={data.imgUrl ?? imagePlaceholder} alt="new" />
       <EventTypeWrapper>{data.type}</EventTypeWrapper>
       <VersinWrapper>{`v${data.version}`}</VersinWrapper>
       <TitleWrapper>{data.title}</TitleWrapper>
