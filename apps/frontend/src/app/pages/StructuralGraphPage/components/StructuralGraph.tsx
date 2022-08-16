4bcc4dfb1a9f17f4e43f194a5a95eb3df0e634e7
MrILL <ilua2403@ukr.net>
2022-08-16 23:55:12 +0300

    fix(frontend): fix deleting edges on setState

--- apps/frontend/src/app/pages/StructuralGraphPage/components/StructuralGraph.tsx
+++ apps/frontend/src/app/pages/StructuralGraphPage/components/StructuralGraph.tsx
@@ -20,6 +20,7 @@ export function StructuralGraph({
 }) {
   const [nodes, setNodes] = React.useState<Node[]>([])
   const [edges, setEdges] = React.useState<Edge[]>([])
+  // console.log(edges)
 
   React.useEffect(() => {
     const builder = new GameEventsBuilder(serverEvents)
@@ -32,51 +33,50 @@ export function StructuralGraph({
 
   const onNodesChange = React.useCallback(
     (changes: NodeChange[]) => {
-      console.log(changes)
+      // console.log(changes)
       const selectChange = changes.find(change => change.type === 'select')
-      if (selectChange && selectChange.type === 'select') {
-        let edgesOfNode
 
-        if (selectChange.selected === true) {
-          edgesOfNode = edges.map(edge => {
-            // if (edge.target === selectChange.id) {
-            //   //TODO update incoming edges
-            //   return { ...edge, stroke: '#9c9998', strokeWidth: 4 }
-            // } else if (edge.source === selectChange.id) {
-            //   //TODO update outcoming edges
-            //   return { ...edge, stroke: '#d9765d', strokeWidth: 4 }
-            // } else {
-            return edge
-            // }
-          })
-        } else {
-          //TODO make all edges default
-          edgesOfNode = edges.map(edge => {
-            // if (
-            //   edge.target === selectChange.id ||
-            //   edge.source === selectChange.id
-            // ) {
-            //   const { stroke, strokeWidth, ...rest } = edge as any
-            //   return rest
-            // } else {
-            return edge
-            // }
-          })
+      const newEdges: any[] = edges.map(edge => {
+        if (!selectChange || selectChange.type !== 'select') {
+          return edge
         }
 
-        setEdges(edgesOfNode)
+        if (selectChange.selected) {
+          if (edge.target === selectChange.id) {
+            //TODO update incoming edges
+            console.log({ ...edge, stroke: '#9c9998', strokeWidth: 4 })
+            return { ...edge, stroke: '#9c9998', strokeWidth: 4 }
+          } else if (edge.source === selectChange.id) {
+            //TODO update outcoming edges
+            console.log({ ...edge, stroke: '#d9765d', strokeWidth: 4 })
+            return { ...edge, stroke: '#d9765d', strokeWidth: 4 }
+          } else {
+            return edge
+          }
+        } else {
+          return edge
+          // if (
+          //   edge.target === selectChange.id ||
+          //   edge.source === selectChange.id
+          // ) {
+          //   const { stroke, strokeWidth, ...rest } = edge as any
+          //   return rest
+          // } else {
+          //   return edge
+          // }
+        }
+      })
 
-        //TODO set edges
-      }
-
-      return setNodes(nds => applyNodeChanges(changes, nds))
+      // console.log(newEdges)
+      setEdges(newEdges)
+      setNodes(nds => applyNodeChanges(changes, nds))
     },
-    [setNodes],
+    [serverEvents, edges, nodes, setNodes, setEdges],
   )
-  const onEdgesChange = React.useCallback(
-    (changes: EdgeChange[]) => setEdges(eds => applyEdgeChanges(changes, eds)),
-    [setEdges],
-  )
+  // const onEdgesChange = React.useCallback(
+  //   (changes: EdgeChange[]) => setEdges(eds => applyEdgeChanges(changes, eds)),
+  //   [setEdges],
+  // )
 
   const nodeTypes = React.useMemo(() => ({ card: EventCard }), [])
 
@@ -90,7 +90,7 @@ export function StructuralGraph({
       nodes={nodes}
       edges={edges}
       onNodesChange={onNodesChange}
-      onEdgesChange={onEdgesChange}
+      // onEdgesChange={onEdgesChange}
       minZoom={0.1}
     />
   )
