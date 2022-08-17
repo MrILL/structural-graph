1052f59fd84cbbc868593911d4cf83fd56bf74ff
MrILL <ilua2403@ukr.net>
2022-08-17 22:40:06 +0300

    fix(frontend): multiple setState but edge highlight remain not working

--- apps/frontend/src/app/pages/StructuralGraphPage/components/StructuralGraph.tsx
+++ apps/frontend/src/app/pages/StructuralGraphPage/components/StructuralGraph.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import ReactFlow, {
-  applyEdgeChanges,
   applyNodeChanges,
   Edge,
   EdgeChange,
@@ -13,65 +12,87 @@ import { GameEvent } from '@sg/types'
 import { EventCard } from './EventCard'
 import { GameEventsBuilder } from '../lib/GameEventsBuilder'
 
+function customApplyEdgeChanges(changes: any[], edges: any[]): any[] {
+  const selectChange = changes.find(change => change.type === 'select')
+
+  const newEdges: any[] = edges.map(edge => {
+    if (!selectChange || selectChange.type !== 'select') {
+      return edge
+    }
+
+    if (selectChange.selected) {
+      if (edge.target === selectChange.id) {
+        //TODO update incoming edges
+        console.log('target:', { ...edge, stroke: '#9c9998', strokeWidth: 4 })
+        return { ...edge, stroke: '#9c9998', strokeWidth: 4 }
+      } else if (edge.source === selectChange.id) {
+        //TODO update outcoming edges
+        console.log('source:', { ...edge, stroke: '#d9765d', strokeWidth: 4 })
+        return { ...edge, stroke: '#d9765d', strokeWidth: 4 }
+      } else {
+        return edge
+      }
+    } else {
+      return edge
+      // if (
+      //   edge.target === selectChange.id ||
+      //   edge.source === selectChange.id
+      // ) {
+      //   const { stroke, strokeWidth, ...rest } = edge as any
+      //   return rest
+      // } else {
+      //   return edge
+      // }
+    }
+  })
+
+  return newEdges
+}
+
 export function StructuralGraph({
   serverEvents,
 }: {
   serverEvents: GameEvent[]
 }) {
-  const [nodes, setNodes] = React.useState<Node[]>([])
-  const [edges, setEdges] = React.useState<Edge[]>([])
+  // const [nodes, setNodes] = React.useState<Node[]>([])
+  // const [edges, setEdges] = React.useState<Edge[]>([])
+  const [state, setState] = React.useState<{
+    nodes: Node[]
+    edges: Edge[]
+  }>({
+    nodes: [],
+    edges: [],
+  })
   // console.log(edges)
+  const { nodes, edges } = state
 
   React.useEffect(() => {
     const builder = new GameEventsBuilder(serverEvents)
 
     const { nodes, edges } = builder.getGraphInput()
 
-    setNodes(nodes)
-    setEdges(edges)
+    setState({ nodes, edges })
+    // setNodes(nodes)
+    // setEdges(edges)
   }, [serverEvents])
 
   const onNodesChange = React.useCallback(
     (changes: NodeChange[]) => {
-      // console.log(changes)
-      const selectChange = changes.find(change => change.type === 'select')
-
-      const newEdges: any[] = edges.map(edge => {
-        if (!selectChange || selectChange.type !== 'select') {
-          return edge
-        }
-
-        if (selectChange.selected) {
-          if (edge.target === selectChange.id) {
-            //TODO update incoming edges
-            console.log({ ...edge, stroke: '#9c9998', strokeWidth: 4 })
-            return { ...edge, stroke: '#9c9998', strokeWidth: 4 }
-          } else if (edge.source === selectChange.id) {
-            //TODO update outcoming edges
-            console.log({ ...edge, stroke: '#d9765d', strokeWidth: 4 })
-            return { ...edge, stroke: '#d9765d', strokeWidth: 4 }
-          } else {
-            return edge
-          }
-        } else {
-          return edge
-          // if (
-          //   edge.target === selectChange.id ||
-          //   edge.source === selectChange.id
-          // ) {
-          //   const { stroke, strokeWidth, ...rest } = edge as any
-          //   return rest
-          // } else {
-          //   return edge
-          // }
-        }
+      console.log(changes)
+      setState({
+        nodes: applyNodeChanges(changes, nodes),
+        edges: customApplyEdgeChanges(changes, edges),
       })
-
-      // console.log(newEdges)
-      setEdges(newEdges)
-      setNodes(nds => applyNodeChanges(changes, nds))
+      // setEdges(edgs => customApplyEdgeChanges(changes, edgs))
+      // setNodes(nds => applyNodeChanges(changes, nds))
     },
-    [serverEvents, edges, nodes, setNodes, setEdges],
+    [
+      state,
+      // edges,
+      // nodes,
+      // setNodes,
+      // setEdges
+    ],
   )
   // const onEdgesChange = React.useCallback(
   //   (changes: EdgeChange[]) => setEdges(eds => applyEdgeChanges(changes, eds)),
