9d037e079de7eece7bf03c0011ddf992b41a40b8
MrILL <ilua2403@ukr.net>
2022-08-25 00:00:47 +0300

    fix(frontend): correct on reselect node edge highlight

--- apps/frontend/src/app/pages/StructuralGraphPage/components/StructuralGraph.tsx
+++ apps/frontend/src/app/pages/StructuralGraphPage/components/StructuralGraph.tsx
@@ -19,36 +19,43 @@ const EDGE_SELECTED_OUTCOMING_COLOR = '#d9765d'
 function customApplyEdgeChanges(changes: any[], edges: any[]): any[] {
   let newEdges = edges
 
-  changes.forEach(change => {
-    if (change.type === 'select') {
-      newEdges = newEdges.map(edge => {
-        if (change.selected) {
-          if (edge.target !== change.id && edge.source !== change.id) {
-            return edge
-          }
+  changes
+    .sort((change1, change2) => {
+      const v1 = change1.type !== 'select' ? 0 : change1.selected ? 2 : 1
+      const v2 = change2.type !== 'select' ? 0 : change2.selected ? 2 : 1
 
-          return {
-            ...edge,
-            style: {
-              stroke:
-                edge.target === change.id
-                  ? EDGE_SELECTED_INCOMING_COLOR
-                  : EDGE_SELECTED_OUTCOMING_COLOR,
-              strokeWidth: 4,
-            },
-          }
-        } else {
-          if (edge.target !== change.id && edge.source !== change.id) {
-            return edge
-          }
+      return v1 - v2
+    })
+    .forEach(change => {
+      if (change.type === 'select') {
+        newEdges = newEdges.map(edge => {
+          if (change.selected) {
+            if (edge.target !== change.id && edge.source !== change.id) {
+              return edge
+            }
 
-          const { style, ...rest } = edge as any
+            return {
+              ...edge,
+              style: {
+                stroke:
+                  edge.target === change.id
+                    ? EDGE_SELECTED_INCOMING_COLOR
+                    : EDGE_SELECTED_OUTCOMING_COLOR,
+                strokeWidth: 4,
+              },
+            }
+          } else {
+            if (edge.target !== change.id && edge.source !== change.id) {
+              return edge
+            }
 
-          return rest
-        }
-      })
-    }
-  })
+            const { style, ...rest } = edge as any
+
+            return rest
+          }
+        })
+      }
+    })
 
   return newEdges
 }
